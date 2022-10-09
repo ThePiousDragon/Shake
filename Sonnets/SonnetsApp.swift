a69003519c6ef42936c112789648093a80727993
ThePiousDragon <auspice-foamier-0f@icloud.com>
2022-10-09 02:37:49 +0100

    Initial Commit

---
+++ Sonnets/SonnetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonnetsApp.swift
+//  Sonnets
+//
+//  Created by dimitar on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SonnetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
